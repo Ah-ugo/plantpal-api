a2080ebb20a67db83bee9b67bcbfb23053e28352
Ahuekwe Prince Ugo <62047075+Ah-ugo@users.noreply.github.com>
2024-12-14 10:42:19 +0100

    Update cloudinary_upload.py

--- Utils/cloudinary_upload.py
+++ Utils/cloudinary_upload.py
@@ -1,6 +1,5 @@
 import cloudinary
 import cloudinary.uploader
-import cloudinary.api
 from fastapi import HTTPException
 import os
 
@@ -10,10 +9,13 @@ cloudinary.config(
     api_secret=os.getenv("API_SECRET")
 )
 
-
-def upload_to_cloudinary(file_path: str) -> str:
-    """
-    Upload a file to Cloudinary and return its URL.
-    """
-    response = cloudinary.uploader.upload(file_path, folder="shops", resource_type="raw")
-    return response.get("url")
+def upload_to_cloudinary(file):
+    try:
+        response = cloudinary.uploader.upload(
+            file.file,
+            folder="shops",
+            resource_type="raw"
+        )
+        return response.get("url")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Cloudinary upload failed: {str(e)}")
